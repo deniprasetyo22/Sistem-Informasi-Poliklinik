fa30b35d0eab2b84ab5d81a61271ee6e4633a307
Deni Prasetyo <19102048@ittelkom-pwt.ac.id>
2024-06-10 16:52:29 +0700

    first commit

--- Control.php
+++ Control.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 session_start();
-$link = mysqli_connect('localhost','root','','epoli');
-function data($query){
+$link = mysqli_connect('localhost', 'root', '', 'epoli');
+function data($query)
+{
 	global $link;
-	$result = mysqli_query($link,$query);
+	$result = mysqli_query($link, $query);
 	return $result;
 }
-
 ?>
+<?php ?>
